68f1aeae432122648aeef60ce6c9ef501dba7dcc
cvvcgituser <115985101+cvvcgituser@users.noreply.github.com>
2022-12-07 14:42:52 +0530

    Update main.tf

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -2,34 +2,34 @@ resource "aws_lb" "external-elb" {
   name               = "${var.external-elb_name}"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.web-sg.id]
-  subnets            = [aws_subnet.web-subnet-1.id, aws_subnet.web-subnet-2.id]
+  security_groups    = [var.elb_sg]
+  subnets            = [var.elb_subnet_1, var.elb_subnet_2]
 }
 
 resource "aws_lb_target_group" "external-elb" {
   name     = "${var.target-group_name}"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.my-vpc.id
+  vpc_id   = var.vpc_id
 }
 
 resource "aws_lb_target_group_attachment" "external-elb1" {
   target_group_arn = aws_lb_target_group.external-elb.arn
-  target_id        = aws_instance.webserver1.id
+  target_id        = var.elb_instance1
   port             = 80
 
   depends_on = [
-    aws_instance.webserver1,
+    var.elb_instance1,
   ]
 }
 
 resource "aws_lb_target_group_attachment" "external-elb2" {
   target_group_arn = aws_lb_target_group.external-elb.arn
-  target_id        = aws_instance.webserver2.id
+  target_id        = var.elb_instance2
   port             = 80
 
   depends_on = [
-    aws_instance.webserver2,
+    var.elb_instance2,
   ]
 }
 
@@ -43,4 +43,5 @@ resource "aws_lb_listener" "external-elb" {
     target_group_arn = aws_lb_target_group.external-elb.arn
   }
 }
+
 
