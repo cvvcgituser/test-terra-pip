6490352b207a2d129c45f8505d2728de4788f45d
cvvcgituser <115985101+cvvcgituser@users.noreply.github.com>
2022-12-07 14:44:06 +0530

    Update variables.tf

--- modules/loadbalancer/variables.tf
+++ modules/loadbalancer/variables.tf
@@ -6,3 +6,9 @@ variable "target-group_name" {
   description = "target group name"
   default     = "ALB-TG"
 }
+variable "elb_sg" {}
+variable "elb_subnet_1" {}
+variable "elb_subnet_2" {}
+variable "vpc_id" {}
+variable "elb_instance1" {}
+variable "elb_instance2" {}
