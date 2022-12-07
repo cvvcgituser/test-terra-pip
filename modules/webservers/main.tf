ab21109e92d7d139cba56f8f3b0c36ce5c994f51
cvvcgituser <115985101+cvvcgituser@users.noreply.github.com>
2022-12-07 14:50:24 +0530

    Update main.tf

--- modules/webservers/main.tf
+++ modules/webservers/main.tf
@@ -3,8 +3,8 @@ resource "aws_instance" "webserver1" {
   ami                    = "${var.webserver1_ami_id}"
   instance_type          = "${var.webserver1_instance_type}"
   availability_zone      = "${var.webserver1_az}"
-  vpc_security_group_ids = [aws_security_group.webserver-sg.id]
-  subnet_id              = aws_subnet.web-subnet-1.id
+  vpc_security_group_ids = [var.webserver1_sg]
+  subnet_id              = var.webserver1_subnet_1
   user_data              = file("install_apache.sh")
 
   tags = {
@@ -17,8 +17,8 @@ resource "aws_instance" "webserver2" {
   ami                    = "${var.webserver2_ami_id}"
   instance_type          = "${var.webserver2_instance_type}"
   availability_zone      = "${var.webserver2_az}"
-  vpc_security_group_ids = [aws_security_group.webserver-sg.id]
-  subnet_id              = aws_subnet.web-subnet-2.id
+  vpc_security_group_ids = [var.webserver1_sg]
+  subnet_id              = var.webserver2_subnet_2
   user_data              = file("install_apache.sh")
 
   tags = {
